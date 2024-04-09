ab20b24dc63412e7a1125047fa1b50af6cf75ea5
rameezrizvan <rameezrizvan04@gmail.com>
2024-04-09 10:15:57 +0530

    New Reactjs project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/sidebar.jsx'
+import Main from './components/Main/Main1.jsx'
+
+
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+export default App
